2740549129a8b7fd176717dfa2ab59659f3afc3e
Son <thswjddn0812@gmail.com>
2025-12-02 14:16:04 +0900

    14:16

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const dogsRouter = require('./routes/dogs');
+const express = require("express");
+const cors = require("cors");
+const dogsRouter = require("./routes/dogs");
 
 const app = express();
 const PORT = 4000;
@@ -9,12 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 // 인덱스 라우트
-app.get('/', (req, res) => {
-  res.send('안녕하세요 ~~~ 서버입니다.');
+app.get("/", (req, res) => {
+  res.send("안녕하세요 ~~~ 서버입니다.");
 });
 
 // dogs 라우터 연결
-app.use('/dogs', dogsRouter);
+app.use("/dogs", dogsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
